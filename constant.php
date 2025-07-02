10e76aecfff0fdbafbc9c5f8e1c699f6c1c94a95
gitchennai1 <gitchennai1@gmail.com>
2025-07-02 12:39:15 +0530

    Update constant.php

--- constant.php
+++ constant.php
@@ -2,7 +2,7 @@
 return [
     'DB_HOST'     => 'localhost',
     'DB_NAME'     => 'todo_app',
-    'DB_USERNAME' => 'root',
-    'DB_PASSWORD' => '',
+    'DB_USERNAME' => 'todo_app',
+    'DB_PASSWORD' => 'W$0VPCXcj?t6kD"V',
     'DB_CHARSET'  => 'utf8mb4',
 ];
