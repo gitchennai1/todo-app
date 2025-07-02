0a1bcf13fd5bce01af329a19211f7d1e5165f5aa
gitchennai1 <gitchennai1@gmail.com>
2025-07-02 12:31:30 +0530

    Updated db.php

--- config/db.php
+++ config/db.php
@@ -1,11 +1,23 @@
 <?php
+
 $host = 'localhost';
 $dbname = 'todo_app';
-$user = 'root';
-$pass = '';
+//$user = 'root';
+//$pass = '';
+$user = 'todo_app';
+$pass = 'W$0VPCXcj?t6kD"V';
+
+$config = require_once 'config/constant.php';
+
+$host     = $config['DB_HOST'];
+$dbname   = $config['DB_NAME'];
+$username = $config['DB_USERNAME'];
+$password = $config['DB_PASSWORD'];
+$charset  = $config['DB_CHARSET'];
+>>>>>>> 0745d3fa73f0157ba9e15646aa0757083363787d
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Connection failed: " . $e->getMessage());
